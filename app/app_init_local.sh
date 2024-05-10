c874fcc71ca0c78b09894c222b43188af5f36703
gkwk <124476558+gkwk@users.noreply.github.com>
2024-05-11 02:20:54 +0900

    [Feat] WAS 실행 자동화 sh 추가 (Local)

---
+++ app/app_init_local.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+ENV_FILE=./.env
+DB_FILE=./volume/database/test.sqlite
+ALEMBIC_TEMPLATE_FOLDER=./database/alembic_template_sqlite
+ALEMBIC_TEMPLATE_FOLDER_ABSOLUTE_PATH=$(realpath "$ALEMBIC_TEMPLATE_FOLDER")
+
+
+if !([ -e $ENV_FILE ]); then
+    echo "env file not found"
+    exit 1
+fi
+
+if [ -e $DB_FILE ]; then
+	python main.py
+else
+    alembic init -t "$ALEMBIC_TEMPLATE_FOLDER_ABSOLUTE_PATH" alembic_migrations
+    alembic revision --autogenerate
+    alembic upgrade head
+    python main.py
+fi
