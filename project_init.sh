c957477e3ccc8b56e45afeb45092093fec745a3c
gkwk <124476558+gkwk@users.noreply.github.com>
2024-05-16 03:03:09 +0900

    [Feat] 프로젝트 실행 관련 스크립트 추가

---
+++ project_init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ENV_FILE=./app/.env
+
+if [ -e $ENV_FILE ]; then
+    sudo mkdir -p volume/database volume/ai_model_store volume/staticfile
+
+    SOURCE_DIR="app/volume/staticfile"
+    DEST_DIR="volume/staticfile"
+
+    sudo cp -r "$SOURCE_DIR"/* "$DEST_DIR/"
+
+    sudo docker-compose up --build
+else
+    echo "env file not found"
+    exit 1
+fi
