b6cc904383c857df92746d1601fbe9b2978b6f1f
gkwk <124476558+gkwk@users.noreply.github.com>
2024-07-03 01:40:54 +0900

    [Modify] config 및 .env 수정

--- app/config/config.py
+++ app/config/config.py
@@ -8,9 +8,13 @@ class Settings(BaseSettings):
     APP_JWT_SECRET_KEY: str
     APP_JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int
     APP_JWT_REFRESH_TOKEN_EXPIRE_MINUTES: int
-    OAUTH_TOKEN_URL : str
+    OAUTH_TOKEN_URL: str
     PASSWORD_ALGORITHM: str
-    SQLALCHEMY_DATABASE_URL: str
+    RDBMS_DRIVER: str
+    RDBMS_HOST_NAME: str
+    RDBMS_USERNAME_ENV: str
+    RDBMS_PASSWORD_ENV: str
+    RDBMS_DB_NAME: str
     REDIS_HOST_NAME: str
 
     model_config = SettingsConfigDict(env_file=".env")
